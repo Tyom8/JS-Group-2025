ae499c2d4f6059e2f0a4145220fdffac831bd761
varspijoyan <varsikpijoyan2003@gmail.com>
2025-05-29 19:26:35 +0400

    Create header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import DashBoardUsers from "./components/DashboardUsers/DashboardUsers";
+import Header from "./components/Header/Header";
 import AddTaskbar from "./components/TaskbarForm/AddTaskbar";
 
 function App() {
   return (
     <>
       {/* <DashBoardUsers /> */}
-      <AddTaskbar />
+      {/* <AddTaskbar /> */}
+      <Header />
     </>
   );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import image from "../../assets/default-image.jpg";
+import { useUserProfileContent } from "../../hooks/ShowUserProfileContent-hook";
+import styles from "../../styles/Header.module.css";
+
+const Header: React.FC = () => {
+  const { isProfileShown, toggle } = useUserProfileContent();
+  return (
+    <header>
+      <div className={styles.container}>
+        <div className={styles.logoContent}>
+          <h1 className={styles.logo}>Trelloid</h1>
+          <div className={styles.languageContent}>
+            <select className={styles.languageSelect}>
+              <option value="en">🇺🇸</option>
+              <option value="hy">🇦🇲</option>
+              <option value="ru">🇷🇺</option>
+            </select>
+          </div>
+        </div>
+        <div className={styles.userContent} onClick={() => toggle()}>
+          <div className={styles.userInfo}>
+            <img src={image} alt="user image" />
+            <p>First name</p>
+          </div>
+          {isProfileShown && (
+            <div className={styles.showedProfile}>
+              <p>change password</p>
+              <p>logout</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
