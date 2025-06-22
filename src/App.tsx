e441d37026ac696cbc9a19822bc082dd9adfb2a6
varspijoyan <varsikpijoyan2003@gmail.com>
2025-06-22 20:09:09 +0400

    Comment line

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
       {/* <AddTaskbar /> */}
       <Header />
       {/* <ProjectsList /> */}
-      <LoginForm />
+      {/* <LoginForm /> */}
       {/* <DashboardPage /> */}
       {/* <Taskbar /> */}
       {/* <AddTaskbar /> */}
