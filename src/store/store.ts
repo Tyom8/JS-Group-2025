2a7e495e1ffd4712f984d9200cd16afbfb8f43a7
varspijoyan <varsikpijoyan2003@gmail.com>
2025-06-26 15:29:19 +0400

    Add routings

--- src/store/store.ts
+++ src/store/store.ts
@@ -14,5 +14,6 @@ const rootReducer = combineReducers({
 const store = createStore(rootReducer);
 
 export type RootState = ReturnType<typeof rootReducer>;
+export type AppDispatch = typeof store.dispatch;
 
-export default store
+export default store;
