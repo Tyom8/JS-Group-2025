af8f9f1ba58ef23fc659893ec419a9c138f37cee
varspijoyan <varsikpijoyan2003@gmail.com>
2025-06-27 12:19:25 +0400

    Add layout page

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
 import { Provider } from "react-redux";
-import { Outlet, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import DashBoardUsers from "./components/DashboardUsers/DashboardUsers";
-import Header from "./components/Header/Header";
 import "./i18n";
 import AddTaskbar from "./pages/AddTaskbar";
+import DashboardPage from "./pages/Dashboard";
+import Layout from "./pages/Layout";
 import LoginForm from "./pages/LoginForm";
 import { useAppSelector } from "./store/hooks";
 import store from "./store/store";
-import DashboardPage from "./pages/Dashboard";
-function Layout() {
-  return (
-    <>
-      <Header />
-      <Outlet />
-    </>
-  );
-}
+
 function AppContent() {
   const { isAuthenticated } = useAppSelector((state) => state.user);
 
@@ -28,8 +20,8 @@ function AppContent() {
     <>
       <Routes>
         <Route path="/login" element={<LoginForm />} />
-        <Route path="/" element={<Layout/>}>
-          <Route index element={<DashboardPage />}/>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<DashboardPage />} />
           <Route path="dashboard" element={<DashboardPage />} />
           <Route path="taskbar" element={<AddTaskbar />} />
         </Route>
@@ -37,6 +29,7 @@ function AppContent() {
     </>
   );
 }
+
 function App() {
   return (
     <Provider store={store}>
